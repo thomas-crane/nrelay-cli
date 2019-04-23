257c443f09f0bee70db4c62432a4070373b085af
thomas-crane <thomasgcrane@gmail.com>
2019-04-23 19:41:54 +1000

    feat(build): add clean stage to build command
    This commit adds a new stage to the build command which removes the
    output directory before the project is built. Building a ts project will
    overwrite existing files, but won't remove old ones. This was causing
    old plugins which had since been deleted to stick around. This is now
    fixed because the build directory is cleaned before the build occurs.

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,10 +1,20 @@
 import execa from 'execa';
 import Listr from 'listr';
+import { rm } from '../util/fs-util';
+import { cwd } from '../util/path-util';
 
 /**
  * Builds the nrelay project.
  */
 export const build = new Listr([
+  {
+    title: 'Clean project',
+    task: () => {
+      // use the outDir from the tsconfig so it's always up to date.
+      const outPath = require(cwd('tsconfig.json')).compilerOptions.outDir;
+      return rm(cwd(outPath));
+    },
+  },
   {
     title: 'Build project',
     task: () => execa('npm', ['run', 'compile']),
